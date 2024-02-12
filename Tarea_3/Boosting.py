8db4210cc1ad03c283a87ce3657ab3d351361747
daniel robayo <18-11086@usb.ve>
2024-02-12 16:38:17 -0400

    smt

---
+++ Tarea_3/Boosting.py
@@ -0,0 +1,129 @@
+from Neural_Network import Neural_Network
+import pandas as pd
+from random import random
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class Boosting:
+    def __init__(self, 
+                 data: pd.DataFrame,
+                 number_of_neurons_hidden_layers: [int],
+                 learning_rate: float = 0.01,
+                 momentum: float = 0.0,
+                 max_epoch: int = 100):
+        N = len(data)
+        n1, n2, n3 = N//4, N//3, N//3
+
+        print('::Expert 1')
+        n1_data = data.sample(n1, ignore_index=True)
+        self.e1 = Neural_Network(data=n1_data,
+                                 number_of_neurons_hidden_layers=number_of_neurons_hidden_layers,
+                                 learning_rate=learning_rate,
+                                 momentum=momentum,
+                                 max_epoch=max_epoch)
+
+        print('::Expert 2')
+        n2_data = self.__get_n2_data(data, n2)
+        self.e2 = Neural_Network(data=n2_data,
+                                 number_of_neurons_hidden_layers=number_of_neurons_hidden_layers,
+                                 learning_rate=learning_rate,
+                                 momentum=momentum,
+                                 max_epoch=max_epoch)
+
+        print('::Expert 3')        
+        n3_data = self.__get_n3_data(data, n3)
+        self.e3 = Neural_Network(data=n3_data,
+                                 number_of_neurons_hidden_layers=number_of_neurons_hidden_layers,
+                                 learning_rate=learning_rate,
+                                 momentum=momentum,
+                                 max_epoch=max_epoch)
+
+        self.__plot_data(data)
+        
+    def __get_n2_data(self, 
+                      data: pd.DataFrame,
+                      n: int) -> pd.DataFrame:
+        n2_examples_index = []
+        example_counter = 0
+
+        is_head = random() >= 0.5
+
+        while True:
+
+            for i, row in data.iterrows():
+                x = row[data.columns[:-1]]
+                x['b'] = 1
+
+                y = row[data.columns[-1]]
+                
+                network_output = self.e1.get_output(x)
+
+                if is_head and y != network_output:
+                    is_head = random() >= 0.5
+                    example_counter += 1
+                    n2_examples_index += [i]
+
+                elif not is_head and y == network_output:
+                    is_head = random() >= 0.5
+                    example_counter += 1
+                    n2_examples_index += [i]
+
+                if example_counter >= n:
+                    return data.iloc[n2_examples_index]
+
+    def __get_n3_data(self,
+                      data: pd.DataFrame,
+                      n: int) -> pd.DataFrame:
+        n3_examples_index = []
+        example_counter = 0
+
+        for i, row in data.iterrows():
+            x = row[data.columns[:-1]]
+            x['b'] = 1
+                
+            network1_output = self.e1.get_output(x)
+            network2_output = self.e2.get_output(x)
+
+            if network1_output != network2_output:
+                example_counter += 1
+                n3_examples_index += [i]
+
+            if example_counter >= n:
+                return data.iloc[n3_examples_index]
+            
+        return data.iloc[n3_examples_index]
+    
+    def get_output(self, x: pd.DataFrame) -> int:
+        output_1 = self.e1.get_output(x)
+        output_2 = self.e2.get_output(x)
+        output_3 = self.e3.get_output(x)
+
+        if output_1 == output_2:
+            return output_2
+        
+        return output_3
+    
+
+    def __plot_data(self, data: pd.DataFrame):
+        committee_y = np.array([])
+        for _, row in data.iterrows():
+            x = row[data.columns[:-1]]
+            x['b'] = 1
+
+            committee_output = self.get_output(x)
+
+            committee_y = np.append(committee_y, committee_output)
+
+        data['committee_y'] = committee_y
+        
+        fig, ax = plt.subplots(ncols=2)
+        sns.scatterplot(data, x='x1', y='x2', hue='y', ax=ax[0])
+        sns.scatterplot(data, x='x1', y='x2', hue='network_y', ax=ax[1])
+        
+        ax[0].set_title('Grafico de dispersion del conjunto de datos')
+        ax[1].set_title('Grafico de dispersion resultante del comite')
+        fig.set_figwidth(10)
+        plt.show()
+
+
--- Tarea_3/Neural_Network.py
+++ Tarea_3/Neural_Network.py
@@ -2,12 +2,14 @@ import pandas as pd
 import numpy as np
 from math import exp
 from sklearn.model_selection import train_test_split
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 class Neuron:
     def __init__(self, 
                  number_of_weights: int):
         self.weigths = np.random.random(number_of_weights + 1)
-        self.input = None
+        self.prev_weigths = np.array([0] * (number_of_weights+1))
 
     def activate(self, input_data: pd.DataFrame) -> float:
         stimulus = np.dot(input_data, self.weigths)
@@ -21,7 +23,9 @@ class Neural_Network:
     def __init__(self,
                  data: pd.DataFrame,
                  number_of_neurons_hidden_layers: int = 0,
-                 learning_rate: float = 0.01):
+                 learning_rate: float = 0.01,
+                 momentum: float = 0.0,
+                 max_epoch: int = 100):
         
         variables = len(data.columns)-1
         
@@ -29,10 +33,15 @@ class Neural_Network:
         self.layers.append([Neuron(number_of_neurons_hidden_layers)])
 
         self.learning_rate = learning_rate
+        self.momentum = momentum
+        self.max_epoch = max_epoch
 
-        train, test = train_test_split(data, train_size=0.8, test_size=0.2)
+        train, test = train_test_split(data, train_size=0.8, test_size=0.2, shuffle=True)
+        
         self.__train(train)
+        self.__plot_error()
         self.__test(test)
+        self.__plot_data(data)
 
     def __feed_fordward(self, x: pd.DataFrame):
         activation_hidden_layer = np.array([])
@@ -72,15 +81,16 @@ class Neural_Network:
         # update output layer
         for i in range(len(self.layers[1][0].weigths)):
             delta_w = self.learning_rate*gradient_output_layer*activation_hidden_layer[i]
-            self.layers[1][0].weigths[i] += delta_w
+            self.layers[1][0].weigths[i] += delta_w + self.momentum*self.layers[1][0].prev_weigths[i]
+            self.layers[1][0].prev_weigths[i] = delta_w + self.momentum*self.layers[1][0].prev_weigths[i]
 
         # update hidden layers
         for i in range(len(self.layers[0])):
             for j in range(len(self.layers[0][i].weigths)):
                 # j_th weight of the i_th neuron in the hidden layer
                 delta_w = self.learning_rate*gradient_hidden_layer[i]*input[j]
-                self.layers[0][i].weigths[j] += delta_w 
-
+                self.layers[0][i].weigths[j] += delta_w + self.momentum*self.layers[0][i].prev_weigths[j]
+                self.layers[0][i].prev_weigths[j] = delta_w + self.momentum*self.layers[0][i].prev_weigths[j]
         
     def __train(self, data: pd.DataFrame):
         current_epoch = 0
@@ -113,15 +123,75 @@ class Neural_Network:
                                       activation_hidden_layer,
                                       gradient_output)
 
-            bad_clasified_examples_per_epoch.append(bad_clasified_examples)
+            percentage_error = 100*bad_clasified_examples/len(data)
 
-            percentage_error = bad_clasified_examples/len(data)
-            if percentage_error <= 0.01 or current_epoch >= 1000:
-                print(f'Convergence reached in {current_epoch} epochs with {percentage_error}')
-                print(bad_clasified_examples_per_epoch[980:])
+            bad_clasified_examples_per_epoch.append(percentage_error)
+
+            if current_epoch >= self.max_epoch:
+                y_count = data['y'].value_counts()
+                print('::Training::')
+                print(f'Examples distribution in training \n {y_count}')
+                print(f'Convergence reached in {current_epoch} epochs \nwith {round(percentage_error)}% of error')
                 break
 
-            print(current_epoch)
+
+            if current_epoch % 20 == 0:
+                print(f'Epoch {current_epoch} - Error {percentage_error} %')
+
+        self.error_per_epoch = bad_clasified_examples_per_epoch
 
     def __test(self, data: pd.DataFrame):
-        return None
+        bad_clasified_examples = 0
+
+        for _, row in data.iterrows():
+            x = row[data.columns[:-1]]
+            x['b'] = 1
+            d = int(row[data.columns[-1]])
+
+            network_output = 1 if self.__feed_fordward(x)[1] >= 0.5 else -1
+
+            if d != network_output:
+                bad_clasified_examples += 1
+        bad_clasified_examples_percentage = 100*bad_clasified_examples/len(data)
+        y_count = data['y'].value_counts()
+        
+        print('\n::Testing::')
+        print(f'Examples distribution in training \n {y_count}')
+        print(f'Number of well clasified examples: {bad_clasified_examples} ({round(bad_clasified_examples_percentage, 2)} %)')
+
+    def __plot_data(self, data: pd.DataFrame):
+        network_y = np.array([])
+        for _, row in data.iterrows():
+            x = row[data.columns[:-1]]
+            x['b'] = 1
+
+            network_output = 1 if self.__feed_fordward(x)[1] >= 0.5 else -1
+
+            network_y = np.append(network_y, int(network_output))
+
+        data['network_y'] = network_y
+
+        fig, ax = plt.subplots(ncols=2)
+
+        sns.scatterplot(data, x='x1', y='x2', hue='y', ax=ax[0])
+        sns.scatterplot(data, x='x1', y='x2', hue='network_y', ax=ax[1])
+        
+        ax[0].set_title('Grafico de dispersion del conjunto de datos')
+        ax[1].set_title('Grafico de dispersion resultante de la red neuronal')
+        fig.set_figwidth(10)
+        plt.show()
+
+    def __plot_error(self):
+        x = np.linspace(1, len(self.error_per_epoch), len(self.error_per_epoch))
+
+        fig, ax = plt.subplots()
+        ax.plot(x, self.error_per_epoch)
+
+        ax.set_title('Error (%) por epoca')
+        ax.set_xlabel('Epoca')
+        ax.set_ylabel('Porcentaje de mal clasificados (%)')
+        plt.show()
+
+
+    def get_output(self, x: pd.DataFrame) -> int:
+        return 1 if self.__feed_fordward(x)[1] >= 0.5 else -1
